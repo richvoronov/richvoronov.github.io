4ab57eecdff39f002ff992b02b036bf2480c8d26
richvoronov <richvoronov@gmail.com>
2021-10-12 10:35:27 +0300

    first commit

---
+++ precache-manifest.52f2431c32a11566825ede2b073e5c0b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "54f01f90264526be70fa",
+    "url": "/static/css/main.807ac7a6.chunk.css"
+  },
+  {
+    "revision": "54f01f90264526be70fa",
+    "url": "/static/js/main.afe862f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a18144bf5e61971c963",
+    "url": "/static/js/2.bc0b3744.chunk.js"
+  },
+  {
+    "revision": "f40fbad44c52bbb5c3d70aaefda9a380",
+    "url": "/static/media/imageCosmos1.f40fbad4.png"
+  },
+  {
+    "revision": "86c3629dae106af634fd0d8a642b4836",
+    "url": "/static/media/ex-link.86c3629d.svg"
+  },
+  {
+    "revision": "a88955b2db31aaf53b3d478a9e58db93",
+    "url": "/static/media/mail.a88955b2.svg"
+  },
+  {
+    "revision": "41140f93e4556246b656ac015231b8c4",
+    "url": "/static/media/telegram.41140f93.svg"
+  },
+  {
+    "revision": "c39c8c8d24b2b57b86bf2b690e2cb5cf",
+    "url": "/static/media/github.c39c8c8d.svg"
+  },
+  {
+    "revision": "2348e24f5a98b5c19e5cd4519ce60c56",
+    "url": "/static/media/ufo1.2348e24f.svg"
+  },
+  {
+    "revision": "a5e1dd2dd0a677d650dc83364e3c203f",
+    "url": "/static/media/ufo2.a5e1dd2d.svg"
+  },
+  {
+    "revision": "8cb8dfe5c1a417cf19597c5af7b3c3ad",
+    "url": "/static/media/ufo3.8cb8dfe5.svg"
+  },
+  {
+    "revision": "7515f8cb12808566ef0473e913ca1524",
+    "url": "/static/media/Chemistry.7515f8cb.svg"
+  },
+  {
+    "revision": "f94510198aedabf84b7d6233881e4827",
+    "url": "/static/media/Chemistry.f9451019.eot"
+  },
+  {
+    "revision": "e215cb091ea972e1d30725a60436eda8",
+    "url": "/static/media/Chemistry.e215cb09.woff"
+  },
+  {
+    "revision": "4a4bf398fd0103b69a61ebc6bde2188b",
+    "url": "/static/media/Chalkduster.4a4bf398.eot"
+  },
+  {
+    "revision": "4a1fdf20399e1551f924b3c52d7f1a6a",
+    "url": "/static/media/Chalkduster.4a1fdf20.woff"
+  },
+  {
+    "revision": "82062d07bbe02588c888ee220648d630",
+    "url": "/static/media/Chalkduster.82062d07.svg"
+  },
+  {
+    "revision": "57a931b52038cd401bb2717f0d3951b9",
+    "url": "/index.html"
+  }
+];
